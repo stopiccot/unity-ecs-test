f55ce17cef8303d8eb45c0ab12889d8ee0777580
Alexey Petruchik <alexey.petruchik@gmail.com>
2020-07-03 14:03:58 +0300

    Initial commit

---
+++ Assets/ColliderMB.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderMB : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("OnCollisionEnter2D");
+        sprite.color = Color.blue;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("OnTriggerEnter2D");
+        sprite.color = Color.green;
+    }
+}
---
+++ Assets/MoveMB.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMB : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        transform.localPosition = transform.localPosition + speed * transform.TransformVector(Vector3.up);
+    }
+}
